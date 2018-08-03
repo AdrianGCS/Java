12bd1691bd231b24f81daf0caeeeaddc8810f371
AdrianGCS <adrian1271997gmail.com>
2018-08-02 18:52:58 -0930

    Pruebas con Sleep y Join

--- Probando los Hilos en java/src/Main.java
+++ Probando los Hilos en java/src/Main.java
@@ -2,10 +2,17 @@
 public class Main {
 
     public static void main(String[] args){
-    Thread hilo = new Proceso("Proceso 1","Paco");
+    Thread hilo1 = new Proceso("Proceso 1","Paco");
     Thread hilo2 = new Proceso("Proceso 2","Leo");
 
-    hilo.start();
+    hilo1.start();
+    try {
+        hilo1.join(); //Join hace que espere a que el Hilo termine para continuar
+    }catch (InterruptedException ie)
+    {
+        ie.printStackTrace();
+    }
     hilo2.start();
+        System.out.println("Sin Join Inicia el Hilo2 y prosigue sin esperar a que termine");
     }
 }
--- Probando los Hilos en java/src/Proceso.java
+++ Probando los Hilos en java/src/Proceso.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class Proceso extends Thread {
 
    private String nombre;
@@ -10,9 +14,16 @@ public class Proceso extends Thread {
 
     }
     public void run()
-    {
+    { System.out.println("Comienza " + this.getName());
         for (int i=0;i<=20;i++){
             System.out.println(this.getName() +nombre);
+            if (nombre=="Paco" && i==10) {
+                try {
+                    Thread.sleep(1000);
+                    }catch (InterruptedException e){}
+
+
+            }
         }
     }
 }
