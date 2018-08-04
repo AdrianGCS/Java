12125a0a4c811b9ba36a0a7e07cd0361b13fde96
AdrianGCS <adrian1271997@gmail.com>
2018-08-04 12:36:05 +0200

    Pruebas para pasar datos de un hilo a otro

--- Probando los Hilos en java/src/Main.java
+++ Probando los Hilos en java/src/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args){
     Thread hilo1 = new Proceso("Proceso 1","Paco");
     Thread hilo2 = new Proceso("Proceso 2","Leo");
-
-    hilo1.start();
+    long InicioHilo = System.currentTimeMillis();
+    int Num1=((Proceso) hilo1).PruebaPasarDatos(InicioHilo);
     try {
         hilo1.join(); //Join hace que espere a que el Hilo termine para continuar
     }catch (InterruptedException ie)
     {
         ie.printStackTrace();
     }
-    hilo2.start();
+    ((Proceso) hilo2).TiempoEstimado(Num1);
         System.out.println("Sin Join Inicia el Hilo2 y prosigue sin esperar a que termine");
     }
 }
--- Probando los Hilos en java/src/Proceso.java
+++ Probando los Hilos en java/src/Proceso.java
@@ -5,25 +5,30 @@
 public class Proceso extends Thread {
 
    private String nombre;
-
+    //private long TiempoIncial
     public Proceso(String msg,String nombre)
     {
 
     super(msg);//Obtiene el nombre que le dimos en el Main
     this.nombre = nombre;//Obtiene la String nombre que le pasamos en el Main
-
+    //this.TiempoIncial=Inicio
     }
-    public void run()
+    public int PruebaPasarDatos(long Inicio)
     { System.out.println("Comienza " + this.getName());
         for (int i=0;i<=20;i++){
             System.out.println(this.getName() +nombre);
             if (nombre=="Paco" && i==10) {
                 try {
-                    Thread.sleep(1000); //para el Hilo durante 1 segundo
+                    Thread.sleep(10000); //para el Hilo durante 1 segundo
                     }catch (InterruptedException e){}
 
 
             }
         }
+        return (int)(System.currentTimeMillis()-Inicio)/1000;
+    }
+    public void TiempoEstimado(int Inicio)
+    {
+        System.out.println("El Tiempo que tardamos en realizar la tarea fue " +Inicio);
     }
 }
